829c88baeede64998940515f8b8a28642dc8d001
mustafahlvc <mustafahlvc@gmail.com>
2018-02-06 18:50:58 +0300

    Simple Page Layout card color fix.

--- src/core/components/FusePageLayouts/FusePageSimple.js
+++ src/core/components/FusePageLayouts/FusePageSimple.js
@@ -60,7 +60,8 @@ const styles = theme => ({
     },
     contentCard                   : {
         display        : 'flex',
-        backgroundColor: 'white',
+        backgroundColor: theme.palette.background.paper,
+        color          : theme.palette.text.paper,
         flexDirection  : 'column',
         flex           : '1 1 auto',
         boxShadow      : '0px 4px 5px -2px rgba(0,0,0,0.2), 0px 7px 10px 1px rgba(0,0,0,0.14), 0px 2px 16px 1px rgba(0,0,0,0.12)',
