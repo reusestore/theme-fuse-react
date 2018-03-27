3090f15cdedb98300d89347e0b3a0098806b3016
mustafahlvc <mustafahlvc@gmail.com>
2018-03-27 14:08:27 +0300

    unnecessary redux store codes cleaned from Analytics Dashboard

--- src/main/content/apps/dashboards/analytics/store/actions/widgets.actions.js
+++ src/main/content/apps/dashboards/analytics/store/actions/widgets.actions.js
@@ -1,8 +1,6 @@
 import axios from 'axios/index';
 
 export const GET_WIDGETS = '[ANALYTICS DASHBOARD APP] GET WIDGETS';
-export const SET_WIDGET1_DATASET = '[ANALYTICS DASHBOARD APP] SET WIDGET1 DATASET';
-export const SET_WIDGET5_DATASET = '[ANALYTICS DASHBOARD APP] SET WIDGET5 DATASET';
 
 export function getWidgets()
 {
@@ -15,19 +13,4 @@ export function getWidgets()
                 payload: response.data
             })
         );
-}
-
-export function setWidget1Dataset(value)
-{
-    return {
-        type   : SET_WIDGET1_DATASET,
-        payload: value
-    };
-}
-export function setWidget5Dataset(value)
-{
-    return {
-        type   : SET_WIDGET5_DATASET,
-        payload: value
-    };
 }
--- src/main/content/apps/dashboards/analytics/store/reducers/widgets.reducer.js
+++ src/main/content/apps/dashboards/analytics/store/reducers/widgets.reducer.js
@@ -1,10 +1,7 @@
 import * as Actions from '../actions';
-import _ from 'lodash';
 
 const initialState = {
-    data                  : null,
-    widget1SelectedDataSet: '2017',
-    widget5SelectedDataSet: 'today'
+    data                  : null
 };
 
 const widgetsReducer = function (state = initialState, action) {
@@ -15,15 +12,6 @@ const widgetsReducer = function (state = initialState, action) {
                 ...state,
                 data: {...action.payload}
             };
-        case Actions.SET_WIDGET1_DATASET:
-            return _.merge({},
-                state,
-                {widget1SelectedDataSet: action.payload}
-            );
-        case Actions.SET_WIDGET5_DATASET:
-            return _.merge({},
-                state,
-                {widget5SelectedDataSet: action.payload});
         default:
             return state;
     }
