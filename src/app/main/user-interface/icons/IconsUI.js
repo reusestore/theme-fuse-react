57e1e9e08aa83ed4f4db7d0ebe9abc552827d3b7
mustafahlvc <mustafahlvc@gmail.com>
2019-01-08 01:41:34 +0300

    IconsUI page refined.

--- src/app/main/user-interface/icons/IconsUI.js
+++ src/app/main/user-interface/icons/IconsUI.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Icon, Input, Typography} from '@material-ui/core';
+import {Button, Icon, Input, MuiThemeProvider, Paper, Typography, withStyles} from '@material-ui/core';
 import {FusePageSimple, FuseAnimate} from '@fuse';
 import axios from 'axios';
 
@@ -43,11 +43,34 @@ class IconsUI extends Component {
         return (
             <FusePageSimple
                 header={
-                    <div className="flex flex-1 items-center justify-between p-24">
+                    <div className="flex flex-1 items-center justify-between p-16 sm:p-24">
 
                         <FuseAnimate>
                             <Typography variant="h6">Icons</Typography>
                         </FuseAnimate>
+
+                        <div className="flex flex-1 items-center justify-center px-12">
+
+                            <MuiThemeProvider theme={this.props.theme}>
+
+                                <Paper className="flex items-center w-full max-w-512 px-8 py-4 rounded-8" elevation={1}>
+
+                                    <Icon className="mr-8" color="action">search</Icon>
+
+                                    <Input
+                                        placeholder="Search..."
+                                        className="flex flex-1"
+                                        disableUnderline
+                                        fullWidth
+                                        value={searchText}
+                                        onChange={this.handleSearch}
+                                        inputProps={{
+                                            'aria-label': 'Search'
+                                        }}
+                                    />
+                                </Paper>
+                            </MuiThemeProvider>
+                        </div>
 
                         <Button
                             className="normal-case"
@@ -61,28 +84,12 @@ class IconsUI extends Component {
                         </Button>
                     </div>
                 }
-                contentToolbar={
-                    <div className="flex flex-1 items-center p-24 max-w-2xl mx-auto">
-                        <Input
-                            placeholder="Search..."
-                            className="pl-16"
-                            disableUnderline
-                            fullWidth
-                            value={searchText}
-                            onChange={this.handleSearch}
-                            inputProps={{
-                                'aria-label': 'Search'
-                            }}
-                        />
-                        <Icon color="action" className="mr-16">search</Icon>
-                    </div>
-                }
                 content={
                     <div className="py-24 max-w-2xl mx-auto">
                         <FuseAnimate animation="transition.slideUpBigIn" delay={300}>
                             <div className="flex flex-wrap justify-center">
                                 {icons.map((item) => (
-                                    <div className="w-128 h-128 p-16 flex flex-col items-center justify-center" key={item.id}>
+                                    <div className="w-160 h-128 p-16 flex flex-col items-center justify-center" key={item.id}>
                                         <Icon className="text-48" color="action">{item.ligatures}</Icon>
                                         <Typography variant="caption" className="mt-4">{item.ligatures}</Typography>
                                     </div>
@@ -96,4 +103,4 @@ class IconsUI extends Component {
     }
 }
 
-export default IconsUI;
+export default withStyles(null, {withTheme: true})(IconsUI);
