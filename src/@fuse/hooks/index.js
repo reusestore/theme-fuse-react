c86e154d379f5db30dee8b812c98952df4c34355
mustafahlvc <mustafahlvc@gmail.com>
2019-04-15 12:50:11 +0300

    hooks-migration: + usePrevious hook added.

--- src/@fuse/hooks/index.js
+++ src/@fuse/hooks/index.js
@@ -1,3 +1,4 @@
 export {default as useForm} from './useForm';
 export {default as useDebounce} from './useDebounce';
 export {default as useTimeout} from './useTimeout';
+export {default as usePrevious} from './usePrevious';
