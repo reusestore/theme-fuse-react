8667c526e031a8cec8b475dca3cc1b98decd7209
mustafahlvc <mustafahlvc@gmail.com>
2019-08-18 05:31:14 +0300

    Fix: Scrumboard Card Dialog toolbar menu items can not open.

--- src/app/main/apps/scrumboard/board/dialogs/card/toolbar/ToolbarMenu.js
+++ src/app/main/apps/scrumboard/board/dialogs/card/toolbar/ToolbarMenu.js
@@ -1,33 +1,26 @@
 import React from 'react';
-import {Popover, ClickAwayListener} from '@material-ui/core';
+import {Popover} from '@material-ui/core';
 
 function ToolbarMenu(props)
 {
     return (
-        <ClickAwayListener onClickAway={props.onClose}>
-            <Popover
-                hideBackdrop={true}
-                open={Boolean(props.state)}
-                anchorEl={props.state}
-                onClose={props.onClose}
-                anchorOrigin={{
-                    vertical  : 'bottom',
-                    horizontal: 'center'
-                }}
-                transformOrigin={{
-                    vertical  : 'top',
-                    horizontal: 'center'
-                }}
-                className="pointer-events-none"
-                classes={{
-                    paper: "pointer-events-auto py-8"
-                }}
-            >
-                <React.Fragment>
-                    {props.children}
-                </React.Fragment>
-            </Popover>
-        </ClickAwayListener>
+        <Popover
+            open={Boolean(props.state)}
+            anchorEl={props.state}
+            onClose={props.onClose}
+            anchorOrigin={{
+                vertical  : 'bottom',
+                horizontal: 'center'
+            }}
+            transformOrigin={{
+                vertical  : 'top',
+                horizontal: 'center'
+            }}
+        >
+            <React.Fragment>
+                {props.children}
+            </React.Fragment>
+        </Popover>
     );
 }
 
