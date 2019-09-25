7c770444fc100f61af0c97f1790f4acaaf29eaf9
mustafahlvc <mustafahlvc@gmail.com>
2019-09-25 10:36:32 +0300

    Fake-db delay option added for testing purposes.

--- src/@fake-db/mock.js
+++ src/@fake-db/mock.js
@@ -1,4 +1,4 @@
 const MockAdapter = require('axios-mock-adapter');
 const axios = require('axios');
-const mock = new MockAdapter(axios);
+const mock = new MockAdapter(axios, { delayResponse: 0 });
 export default mock;
