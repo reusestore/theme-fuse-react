663551b005977ba6be8ddc0a4bc61b2afb39ac5c
mustafahlvc@gmail.com <mustafahlvc@gmail.com>
2020-01-15 23:21:45 +0300

    CalendarApp IE11 fix.

--- src/app/main/apps/calendar/CalendarApp.js
+++ src/app/main/apps/calendar/CalendarApp.js
@@ -248,3 +248,22 @@ function CalendarApp(props)
 }
 
 export default withReducer('calendarApp', reducer)(CalendarApp);
+
+/*
+IE 11 Fix
+*/
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+  
+if (!Element.prototype.closest) {
+Element.prototype.closest = function(s) {
+    var el = this;
+
+    do {
+    if (el.matches(s)) return el;
+    el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+};
+}
