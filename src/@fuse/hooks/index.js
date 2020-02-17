8c1072873bb0fad3e16974cebc59dd7ffba695f0
mustafahlvc <mustafahlvc@gmail.com>
2020-02-17 17:55:01 +0300

    useDeepCompareEffect hook added.

--- src/@fuse/hooks/index.js
+++ src/@fuse/hooks/index.js
@@ -3,3 +3,4 @@ export { default as useDebounce } from './useDebounce';
 export { default as useTimeout } from './useTimeout';
 export { default as usePrevious } from './usePrevious';
 export { default as useUpdateEffect } from './useUpdateEffect';
+export { default as useDeepCompareEffect } from './useDeepCompareEffect';
