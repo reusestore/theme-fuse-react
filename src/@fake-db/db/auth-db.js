cd9c601422cfd712f4c35765b01101be42827b5d
mustafahlvc <mustafahlvc@gmail.com>
2021-12-15 18:07:38 +0300

    react-scripts updated to v5

--- src/@fake-db/db/auth-db.js
+++ src/@fake-db/db/auth-db.js
@@ -1,13 +1,11 @@
 import FuseUtils from '@fuse/utils';
 import _ from '@lodash';
-import jwt from 'jsonwebtoken';
 import mock from '../mock';
+import Base64 from 'crypto-js/enc-base64';
+import HmacSHA256 from 'crypto-js/hmac-sha256';
+import Utf8 from 'crypto-js/enc-utf8';
+import jwtDecode from 'jwt-decode';
 /* eslint-disable camelcase */
-
-const jwtConfig = {
-	secret: 'some-secret-code-goes-here',
-	expiresIn: '2 days' // A numeric value is interpreted as a seconds count. If you use a string be sure you provide the time units (days, hours, etc)
-};
 
 const authDB = {
 	users: [
@@ -96,7 +94,7 @@ const authDB = {
 	]
 };
 
-mock.onGet('/api/auth').reply(config => {
+mock.onGet('/api/auth').reply(async (config) => {
 	const data = JSON.parse(config.data);
 	const { email, password } = data;
 	const user = _.cloneDeep(authDB.users.find(_user => _user.data.email === email));
@@ -117,10 +115,11 @@ mock.onGet('/api/auth').reply(config => {
 		});
 	}
 
+
 	if (error.length === 0) {
 		delete user.password;
 
-		const access_token = jwt.sign({ id: user.uuid }, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
+		const access_token = generateJWTToken({ id: user.uuid });
 
 		const response = {
 			user,
@@ -137,13 +136,14 @@ mock.onGet('/api/auth/access-token').reply(config => {
 	const data = JSON.parse(config.data);
 	const { access_token } = data;
 
-	try {
-		const { id } = jwt.verify(access_token, jwtConfig.secret);
+	if(verifyJWTToken(access_token)) {
+		const { id } = jwtDecode(access_token);
 
 		const user = _.cloneDeep(authDB.users.find(_user => _user.uuid === id));
+
 		delete user.password;
 
-		const updatedAccessToken = jwt.sign({ id: user.uuid }, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
+		const updatedAccessToken = generateJWTToken({ id: user.uuid });
 
 		const response = {
 			user,
@@ -151,7 +151,7 @@ mock.onGet('/api/auth/access-token').reply(config => {
 		};
 
 		return [200, response];
-	} catch (e) {
+	} else{
 		const error = 'Invalid access token detected';
 		return [401, { error }];
 	}
@@ -188,9 +188,10 @@ mock.onPost('/api/auth/register').reply(request => {
 		authDB.users = [...authDB.users, newUser];
 
 		const user = _.cloneDeep(newUser);
+
 		delete user.password;
 
-		const access_token = jwt.sign({ id: user.uuid }, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn });
+		const access_token = generateJWTToken({ id: user.uuid });
 
 		const response = {
 			user,
@@ -215,3 +216,79 @@ mock.onPost('/api/auth/user/update').reply(config => {
 
 	return [200, user];
 });
+
+/**
+ * JWT Token Generator/Verifier Helpers
+ * !! Created for Demonstration Purposes, cannot be used for PRODUCTION
+ */
+
+const jwtSecret= 'some-secret-code-goes-here';
+
+function base64url(source)
+{
+	// Encode in classical base64
+	let encodedSource = Base64.stringify(source);
+
+	// Remove padding equal characters
+	encodedSource = encodedSource.replace(/=+$/, '');
+
+	// Replace characters according to base64url specifications
+	encodedSource = encodedSource.replace(/\+/g, '-');
+	encodedSource = encodedSource.replace(/\//g, '_');
+
+	// Return the base64 encoded string
+	return encodedSource;
+}
+
+function generateJWTToken(tokenPayload)
+{
+	// Define token header
+	const header = {
+		alg: 'HS256',
+		typ: 'JWT'
+	};
+
+	// Calculate the issued at and expiration dates
+	const date = new Date();
+	const iat = Math.floor(date.getTime() / 1000);
+	const exp = Math.floor((date.setDate(date.getDate() + 7)) / 1000);
+
+	// Define token payload
+	const payload = {
+		iat: iat,
+		iss: 'Fuse',
+		exp: exp,
+		...tokenPayload
+	};
+
+	// Stringify and encode the header
+	const stringifiedHeader = Utf8.parse(JSON.stringify(header));
+	const encodedHeader = base64url(stringifiedHeader);
+
+	// Stringify and encode the payload
+	const stringifiedPayload = Utf8.parse(JSON.stringify(payload));
+	const encodedPayload = base64url(stringifiedPayload);
+
+	// Sign the encoded header and mock-api
+	let signature: any = encodedHeader + '.' + encodedPayload;
+	signature = HmacSHA256(signature, jwtSecret);
+	signature = base64url(signature);
+
+	// Build and return the token
+	return encodedHeader + '.' + encodedPayload + '.' + signature;
+}
+
+function verifyJWTToken(token)
+{
+	// Split the token into parts
+	const parts = token.split('.');
+	const header = parts[0];
+	const payload = parts[1];
+	const signature = parts[2];
+
+	// Re-sign and encode the header and payload using the secret
+	const signatureCheck = base64url(HmacSHA256(header + '.' + payload, jwtSecret));
+
+	// Verify that the resulting signature is valid
+	return (signature === signatureCheck);
+}
