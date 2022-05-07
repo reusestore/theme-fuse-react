1ebe41dadc884dccbcabe4bfde3cdda412eba70f
mustafa <mustafahlvc@gmail.com>
2022-05-07 20:01:03 +0300

    react update to v18, react-scrits updated to v5.0.1

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './i18n';
 import './styles/app-base.css';
 import './styles/app-components.css';
 import './styles/app-utilities.css';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app/App';
 import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 reportWebVitals();
 
