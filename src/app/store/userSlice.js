ac141d4e63cc3d3c18d15b1a80f38c1b8c2378bc
mustafa <mustafahlvc@gmail.com>
2022-05-07 09:16:28 +0300

    craco replaced with react-app-rewired

--- src/app/store/userSlice.js
+++ src/app/store/userSlice.js
@@ -1,6 +1,5 @@
 /* eslint import/no-extraneous-dependencies: off */
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import 'firebase/compat/auth';
 import history from '@history';
 import _ from '@lodash';
 import { setInitialSettings } from 'app/store/fuse/settingsSlice';
